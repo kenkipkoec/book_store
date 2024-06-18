60347d622ca042b93e412eb964404c2bc6830bd1
kenkipkoec <itsmetrokenaki@gmal.com>
2024-06-18 09:58:23 +0300

    project completed

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
+import sqlite3
+
 def main():
+    def display_books(cursor):
+        # Function to display books in a table format
+        cursor.execute("SELECT * FROM books")
+        books = cursor.fetchall()
+        if books:
+            print("{:<30} {:<30} {:<10}".format("Book Name", "Author", "Pages"))
+            print("-" * 70)
+            for book in books:
+                print("{:<30} {:<30} {:<10}".format(book[0], book[1], book[2]))
+        else:
+            print("No books available to display.")
 
- try:
-    # initialize books list
-    booksList = []
-    infile = open("theBooksLists.txt", "r")
-    line = infile.readline()
-    while line:
-        booksList.append(line.rstrip("\n").split
-        (","))
-        line = infile.readline()
-    infile.close()
- except FileNotFoundError :
-    print("the <bookslist.txt> file is not found")
+    # Connect to SQLite database
+    conn = sqlite3.connect("books.db")
+    cursor = conn.cursor()
 
-    print("Starting a new books lists!")
-    booksList = []
+    # Create the books table if it does not exist
+    cursor.execute('''CREATE TABLE IF NOT EXISTS books
+                      (name TEXT, author TEXT, pages INTEGER)''')
 
-
-
-    choice = 0 
-    while choice !=4:
-        print("***Books Manager ***")
-        print ("1) Add a book")
+    choice = 0
+    while choice != 4:
+        print("\n***Books Manager ***")
+        print("1) Add a book")
         print("2) Lookup a book")
         print("3) Display books")
         print("4) Quit")
-        choice = int(input())
+        choice = int(input("Enter your choice: "))
 
         if choice == 1:
             print("Adding a book...")
-            nBook = input("Enter the name of the book>>>")
-
-            nAuthor = input("Enter the name of theauthor>>>")
-    
-            nPages = input("Enter the number of pages >>>")
-
-            booksList.append([nBook, nAuthor, nPages])
-
+            n_book = input("Enter the name of the book: ")
+            n_author = input("Enter the name of the author: ")
+            n_pages = input("Enter the number of pages: ")
+            cursor.execute("INSERT INTO books (name, author, pages) VALUES (?, ?, ?)", (n_book, n_author, n_pages))
+            conn.commit()
+            print("Book added successfully!")
         elif choice == 2:
-            print("Looking ip for a book ...")
-            keyword = input("Enter search Term: ")
-            for book in booksList:
-                if keyword in book:
-                   print(book)
-
+            print("Looking up a book...")
+            keyword = input("Enter search term: ")
+            cursor.execute("SELECT * FROM books WHERE name LIKE ? OR author LIKE ? OR pages LIKE ?", ('%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%'))
+            books = cursor.fetchall()
+            if books:
+                for book in books:
+                    print("{:<30} {:<30} {:<10}".format(book[0], book[1], book[2]))
+            else:
+                print("No book found with the given keyword.")
         elif choice == 3:
             print("Displaying all books...")
-            for i in range(len(booksList)):
-                print(booksList[i])
-        
+            display_books(cursor)
         elif choice == 4:
-            print("Quiting Program")
-    print("Program Terminatred!")
-    
+            print("Quitting Program")
+        else:
+            print("Invalid choice. Please try again.")
 
-    #saving to external TXT file
-    outfile = open("theBooksLists.txt","w")
-    for book in booksList:
-        outfile.write(",".join(book) + "\n")
-    outfile.close()
+    print("Program Terminated!")
+    # Close the database connection
+    conn.close()
 
 if __name__ == "__main__":
-     main()
+    main()
